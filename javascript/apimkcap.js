a1c81a3a57ea6a0096237841b7dfea6c979c1e78
Lucas Fernandez <lucasfernandezc@outlook.com>
2023-09-23 07:42:44 -0300

    corrigiendo errores de codigo mejorando readme subiendo cambios

--- javascript/apimkcap.js
+++ javascript/apimkcap.js
@@ -1,3 +1,5 @@
+
+
 const apiUrl = 'https://api.coingecko.com/api/v3/global';
 
 fetch(apiUrl)
@@ -12,4 +14,29 @@ fetch(apiUrl)
         const cryptocurrencies = Object.keys(marketCapPercentageData);
         const percentages = Object.values(marketCapPercentageData);
         const colors = [
-            'darkred', 'bluu
+            'darkred', 'blue', 'green', 'orange', 'purple',
+            'pink', 'cyan', 'yellow', 'magenta', 'grey'
+        ];
+
+        const ctx = document.getElementById('pieChart').getContext('2d');
+
+        new Chart(ctx, {
+            type: 'doughnut',
+            data: {
+                labels: cryptocurrencies, // Nombres de las criptomonedas
+                datasets: [{
+                    data: percentages, // Porcentajes
+                    backgroundColor: colors, // Colores
+                    borderWidth: 0,
+                    
+                }]
+            },
+            options: {
+                maintainAspectRatio: false,
+                responsive: true,
+            }
+        });
+    })
+    .catch((error) => {
+        console.error('Error al obtener datos globales:', error);
+    });
--- javascript/price.js
+++ javascript/price.js
@@ -1,3 +1,4 @@
+
 // Definir la URL de la API
 const priceUrl = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd';
 
@@ -11,4 +12,18 @@ fetch(priceUrl)
         // Parsear la respuesta JSON
         return response.json();
     })
-    .then((data) ;
+    .then((data) => {
+        // Aquí puedes trabajar con los datos obtenidos de la API
+        console.log('Datos de la API:', data);
+
+        // Por ejemplo, para acceder al precio en USD de Bitcoin:
+        const precioBitcoinUSD = data.bitcoin.usd;
+        console.log('Precio de Bitcoin en USD:', precioBitcoinUSD);
+        const precioBitcoinElement = document.getElementById('infoDisplay');
+        precioBitcoinElement.textContent = `Bitcoin $${precioBitcoinUSD}`;
+    })
+    .catch((error) => {
+        // Manejar errores en caso de que la solicitud falle
+        console.error('Error al obtener datos de la API:', error);
+    });
+
